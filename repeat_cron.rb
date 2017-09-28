d8dc40fe64f2c17ba82219d99ae7baed2f823f9f
Chris O'Sullivan <thechrisoshow@gmail.com>
2017-09-28 10:14:41 +0100

    Update to new python runtime

--- repeat_cron.rb
+++ repeat_cron.rb
@@ -2,7 +2,7 @@ require 'uri'
 require 'net/http'
 
 while true do
-  url = "http://display-kml.appspot.com/purge"
+  url = "http://display-kml-hrd.appspot.com/purge"
   r = Net::HTTP.get_response(URI.parse(url).host, URI.parse(url).path)
   puts r
 end
